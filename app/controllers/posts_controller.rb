5a26f5354ee547deb8cdcbb32a3eb8d62b4959aa
mostafa424 <mostafa.eltawy@gmail.com>
2023-07-25 21:27:40 +0300

    develop complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
 class PostsController < ApplicationController
+def index
+    @posts = Post.all
+end
+
+def new
+    @post = Post.new
+end
+
+def show
+    @post = Post.find(params[:id])
+end
+
+def edit
+@post = Post.find(params[:id])
+end
+
+def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+        redirect_to @post
+    else
+        render 'edit'
+    end
+end
+
+def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Object successfully created"
+      redirect_to @post
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+end
+
+def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+
+end
+private
+def post_params
+    params.require(:post).permit(:title,:content)
+end
+
 end
--- db/migrate/20230724155838_create_posts.rb
+++ db/migrate/20230724155838_create_posts.rb
@@ -3,8 +3,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.string :content
-      t.string :text
-
+      
       t.timestamps
     end
   end
